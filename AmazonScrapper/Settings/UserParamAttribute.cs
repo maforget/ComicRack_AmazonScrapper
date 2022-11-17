64fb10c414e5d0767de424583b058facfe6ad32b
maforget <11904426+maforget@users.noreply.github.com>
2022-11-17 02:50:14 -0500

    Use auto-property

--- AmazonScrapper/Settings/UserParamAttribute.cs
+++ AmazonScrapper/Settings/UserParamAttribute.cs
@@ -9,18 +9,11 @@ namespace AmazonScrapper.Settings
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     sealed class UserParamAttribute : Attribute
     {
-        // See the attribute guidelines at 
-        //  http://go.microsoft.com/fwlink/?LinkId=85236
-        readonly string displayText;
+        public string DisplayText { get; }
 
         public UserParamAttribute(string displayText)
         {
-            this.displayText = displayText;
-        }
-
-        public string DisplayText
-        {
-            get { return displayText; }
+            this.DisplayText = displayText;
         }
     }
 }
