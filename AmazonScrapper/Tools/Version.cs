4bd7fcba6364158ae4850de0a132e2dcf529e782
maforget <11904426+maforget@users.noreply.github.com>
2022-12-09 22:21:05 -0500

    removed parameters from the GetCurrentTag function

--- AmazonScrapper/Tools/Version.cs
+++ AmazonScrapper/Tools/Version.cs
@@ -18,14 +18,16 @@ namespace AmazonScrapper.Tools
 
             var isDirty = !string.IsNullOrEmpty(Properties.Resources.isDirty.Trim());
             var currentCommit = $"{Properties.Resources.CurrentCommit?.Substring(0, 7)}{(isDirty ? "-dirty" : "")}";
-            var currentTag = GetCurrentTag(Properties.Resources.Tags.Trim(), Properties.Resources.CurrentCommit.Trim());
+            var currentTag = GetCurrentTag();
             var isFullRelease = !string.IsNullOrEmpty(currentTag);
 
             return $"{name} v{ver}{(isFullRelease ? "" : " [" + currentCommit + "]")}";
         }
 
-        private static string GetCurrentTag(string tags, string currentCommit)
+        private static string GetCurrentTag()
         {
+            var tags = Properties.Resources.Tags.Trim();
+            var currentCommit = Properties.Resources.CurrentCommit.Trim();
             var dict = new Dictionary<string, string>();
             var sr = new StringReader(tags);
 
