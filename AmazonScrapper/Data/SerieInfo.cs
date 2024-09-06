512737faaec00351e55c82fa03eec24c9df5ad6f
maforget <11904426+maforget@users.noreply.github.com>
2024-09-05 22:16:54 -0400

    Series Regex are now in their own functions.

--- AmazonScrapper/Data/SerieInfo.cs
+++ AmazonScrapper/Data/SerieInfo.cs
@@ -31,41 +31,61 @@ namespace AmazonScrapper.Data
                 return null;
 
             serieInfo.RawText = text;
-            //Expecting Pattern: "Book 5 of 19: Farmhand"
-            Match regex1 = Regex.Match(text, @"Book\s+(?<number>\d+)\s+of\s+(?<count>\d+)\:\s+(?<title>.+)", RegexOptions.IgnoreCase);
-            //Expecting Pattern: "Part of: Usagi Yojimbo Saga (10 Books)"
-            //Expecting Pattern: "Part of: The Way of the Househusband"
-            Match regex2 = Regex.Match(text, @"Part\s+of\:\s*(?<title>(?:[^(]|$)+)(?:\((?<count>\d+)\sBooks\))*", RegexOptions.IgnoreCase);
-            //Expecting Pattern: "Chapter 9 of 9: What I Love About You"
-            Match regex3 = Regex.Match(text, @"Chapter\s+(?<number>\d+)\s+of\s+(?<count>\d+)\:\s+(?<title>.+)", RegexOptions.IgnoreCase);
-            //Expecting Pattern: "Volume 24 of 24: One-Punch Man"
-            Match regex4 = Regex.Match(text, @"Volume\s+(?<number>\d+)\s+of\s+(?<count>\d+)\:\s+(?<title>.+)", RegexOptions.IgnoreCase);
-            //Excepting Pattern: "Related to: My Hero Academia"
-            Match regex5 = Regex.Match(text, @"Related\s+to\:\s*(?<title>(?:[^(]|$)+)(?:\((?<count>\d+)\sBooks\))*", RegexOptions.IgnoreCase);
-            Match regex = regex1.Success ? regex1 : regex2.Success ? regex2 : regex3.Success ? regex3 : regex4.Success ? regex4 : regex5;
+			Match regex = GetSeriesRegex(text);
 
-            string serie = regex.Groups["title"]?.Value?.Trim();
+            string serie = regex?.Groups["title"]?.Value?.Trim();
             serieInfo.Serie = serie;
 
             string number = Parser.Number.ParseFromTitle(bookTitle);//Take the number from the title
-            string altNumber = regex.Groups["number"]?.Value?.Trim();//Take the number in series title
+            string altNumber = regex?.Groups["number"]?.Value?.Trim();//Take the number in series title
             serieInfo.Number = string.IsNullOrEmpty(number) ? altNumber?.TrimStart('0') : number?.TrimStart('0');//use the series title if no book number in title
 
-            string count = regex.Groups["count"]?.Value?.Trim();
+            string count = regex?.Groups["count"]?.Value?.Trim();
             if (int.TryParse(count, out int c))
                 serieInfo.Count = c;
 
             return serieInfo;
         }
 
-        /// <summary>
-        /// Used when Parsing from the Collection Page
-        /// </summary>
-        /// <param name="index">the book number</param>
-        /// <param name="title">the series title</param>
-        /// <param name="count">the number of books int he series</param>
-        /// <returns>a SeriesInfo</returns>
-        public static SerieInfo Parse(string index, string title, string count, AmazonLinkSerie amazonLinkSerie = null)
+		private static Match GetSeriesRegex(string text)
+		{
+            var regexPatterns = new[]
+            {
+			    //Expecting Pattern: "Book 5 of 19: Farmhand"
+			    @"Book\s+(?<number>\d+)\s+of\s+(?<count>\d+)\:\s+(?<title>.+)",
+			    //Expecting Pattern: "Part of: Usagi Yojimbo Saga (10 Books)"
+			    //Expecting Pattern: "Part of: The Way of the Househusband"
+			    @"Part\s+of\:\s*(?<title>(?:[^(]|$)+)(?:\((?<count>\d+)\sBooks\))*",
+			    //Expecting Pattern: "Chapter 9 of 9: What I Love About You"
+			    @"Chapter\s+(?<number>\d+)\s+of\s+(?<count>\d+)\:\s+(?<title>.+)",
+			    //Expecting Pattern: "Volume 24 of 24: One-Punch Man"
+			    @"Volume\s+(?<number>\d+)\s+of\s+(?<count>\d+)\:\s+(?<title>.+)",
+			    //Excepting Pattern: "Related to: My Hero Academia"
+			    @"Related\s+to\:\s*(?<title>(?:[^(]|$)+)(?:\((?<count>\d+)\sBooks\))*",
+            };
+
+			Match regex = null;
+			foreach (var pattern in regexPatterns)
+			{
+				var match = Regex.Match(text, pattern, RegexOptions.IgnoreCase);
+				if (match.Success)
+				{
+					regex = match;
+					break;
+				}
+			}
+
+            return regex;
+		}
+
+		/// <summary>
+		/// Used when Parsing from the Collection Page
+		/// </summary>
+		/// <param name="index">the book number</param>
+		/// <param name="title">the series title</param>
+		/// <param name="count">the number of books int he series</param>
+		/// <returns>a SeriesInfo</returns>
+		public static SerieInfo Parse(string index, string title, string count, AmazonLinkSerie amazonLinkSerie = null)
         {
             SerieInfo serieInfo = new SerieInfo();
             serieInfo.SerieLink = amazonLinkSerie;
