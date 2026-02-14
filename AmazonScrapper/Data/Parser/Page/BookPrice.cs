c5aadee2b8f162c7ef1e10e9cb7f95ba2c580907
maforget <11904426+maforget@users.noreply.github.com>
2026-02-13 21:12:50 -0500

    Added additional price xpath

--- AmazonScrapper/Data/Parser/Page/BookPrice.cs
+++ AmazonScrapper/Data/Parser/Page/BookPrice.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 using System.Windows.Forms;
+using AmazonScrapper.Web;
 
 namespace AmazonScrapper.Data.Parser.Page
 {
@@ -21,13 +22,18 @@ namespace AmazonScrapper.Data.Parser.Page
         {
             var buyOneClick = Node.SelectNodes(".//form[@id='buyOneClick']//input[@name='displayedPrice']/@value")?.FirstOrDefault()?.Attributes["value"];
             var digital_bulk_form = Node.SelectNodes(".//form[@class='digital-bulk-form']//input[@name='displayedPrice']/@value")?.FirstOrDefault()?.Attributes["value"];
+            var slot_price = Node.SelectNodes("//span[@class='slot-price']//text()").Where(x => !string.IsNullOrEmpty(x.InnerText.Trim())).FirstOrDefault().InnerText.Trim();
             var displayedPrice = digital_bulk_form ?? buyOneClick;
 
-            if (displayedPrice == null)
+            if (displayedPrice == null && string.IsNullOrEmpty(slot_price))
                 return -1f;
 
-            string price = displayedPrice.Value.Trim();
+            string price = displayedPrice?.Value.Trim();
             if (float.TryParse(price, out float value))
+                return value;
+
+            Regex priceRegex = new Regex(@"[0-9]+[\.,][0-9]{2}", RegexOptions.IgnoreCase);
+            if (priceRegex.Match(slot_price).Success && float.TryParse(priceRegex.Match(slot_price)?.Value, out value))
                 return value;
 
             return -1f;
